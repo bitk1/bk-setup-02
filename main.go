d56056f09a86691bad1208e33200af3044b237b2
Tom J <tomj@Bitk-Air-2.lan>
2024-06-22 09:05:11 +0100

    Add web GUI for IPFS interface

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
     "encoding/json"
     "fmt"
+    "html/template"
     "io/ioutil"
     "log"
     "net/http"
     "os"
     "os/exec"
 )
+
+var templates = template.Must(template.ParseFiles("index.html"))
 
 func main() {
     http.HandleFunc("/", homeHandler)
@@ -21,45 +24,48 @@ func main() {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "IPFS Interface")
+    if r.Method == http.MethodGet {
+        templates.ExecuteTemplate(w, "index.html", nil)
+    } else {
+        http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+    }
 }
 
 func addFileHandler(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPost {
+    if r.Method == http.MethodPost {
+        file, _, err := r.FormFile("file")
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        defer file.Close()
+
+        tempFile, err := ioutil.TempFile("", "upload-*.tmp")
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        defer os.Remove(tempFile.Name())
+
+        fileBytes, err := ioutil.ReadAll(file)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        tempFile.Write(fileBytes)
+
+        cmd := exec.Command("ipfs", "add", tempFile.Name())
+        output, err := cmd.CombinedOutput()
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        json.NewEncoder(w).Encode(map[string]string{"result": string(output)})
+    } else {
         http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-        return
     }
-
-    file, _, err := r.FormFile("file")
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    defer file.Close()
-
-    tempFile, err := ioutil.TempFile("", "upload-*.tmp")
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    defer os.Remove(tempFile.Name())
-
-    fileBytes, err := ioutil.ReadAll(file)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    tempFile.Write(fileBytes)
-
-    cmd := exec.Command("ipfs", "add", tempFile.Name())
-    output, err := cmd.CombinedOutput()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(map[string]string{"result": string(output)})
 }
 
 func catFileHandler(w http.ResponseWriter, r *http.Request) {
